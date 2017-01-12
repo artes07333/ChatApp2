cfd5c48d495e7b9ba813631d9942fdb6865d69d1
artes07333 <artes07333@i.ua>
2017-01-12 17:07:30 +0200

    ChatApp2

---
+++ ChatAppServer/src/chatappserver/ChatAppServer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatappserver;
+
+import java.net.ServerSocket;
+
+/**
+ *
+ * @author inc07hp
+ */
+public class ChatAppServer {
+
+    private final ServerSocket server;
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
