3d607efbde2bbd803d60c927316ae2742e86ec28
artes07333 <artes07333@i.ua>
2017-01-12 18:47:54 +0200

    ChatApp2 v0.0.1 (server+client)

--- ChatAppClient/src/chatappclient/ChatAppClient.java
+++ ChatAppClient/src/chatappclient/ChatAppClient.java
@@ -5,17 +5,43 @@
  */
 package chatappclient;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author inc07hp
  */
 public class ChatAppClient {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static Socket socket;
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        connect();
+        end();
+    }
+    
+    private static void connect(){
+        try {
+            socket = new Socket("localhost", 8888);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
+    private static void handle(){
+        
+    }
+    
+    private static void end(){
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
     
 }
